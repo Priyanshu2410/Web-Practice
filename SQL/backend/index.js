f7c2230094b7215fea6df6d40513b1f9203b1824
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-07-19 22:58:54 +0530

    sql

---
+++ SQL/backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "employeesystem",
+});
+
+app.post("/create", (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+  
+    db.query(
+      "INSERT INTO employee (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+      [name, age, country, position, wage],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
+        }
+      }
+    );
+  });
+
+app.listen(5000, () => {
+    console.log("Yey, your server is running on port 5000");
+  });
---
+++ SQL/frontend/src/App.js
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import Axios from 'axios';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:5000/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      console.log("success");
+    });
+  };
+
+ 
+  return (
+    <div className="App">
+      <div>
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      </div>
+      </div>
+  );
+}
+
+export default App;
